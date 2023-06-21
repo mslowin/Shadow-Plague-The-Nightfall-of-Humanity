873f07cfb09acbf57f9ec57f77f3419a3abd7630
mslowin <slowinski.michal@outlook.com>
2023-06-22 00:59:29 +0200

    final

--- Assets/Scripts/MonsterDieEndingCutscene.cs
+++ Assets/Scripts/MonsterDieEndingCutscene.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MonsterDieEndingCutscene : MonoBehaviour
 {
@@ -24,11 +25,18 @@ public class MonsterDieEndingCutscene : MonoBehaviour
     /// </summary>
     public float timeToDoorOpen;
 
+    /// <summary>
+    /// Time when the credits scene should be loaded.
+    /// </summary>
+    public float timeToCredits;
+
     private void Start()
     {
+        FindObjectOfType<AudioManager>().Play("Dial_Ending");
         monster.SetActive(false); // if it wasn't false by mistake
         StartCoroutine(cutscene());
         StartCoroutine(door());
+        StartCoroutine(credits());
     }
 
     IEnumerator cutscene()
@@ -41,5 +49,15 @@ public class MonsterDieEndingCutscene : MonoBehaviour
     {
         yield return new WaitForSeconds(timeToDoorOpen);
         doorAnimator.SetTrigger("open");
+    }
+
+    IEnumerator credits()
+    {
+        yield return new WaitForSeconds(timeToCredits);
+        PlayerPrefs.SetInt("continue", 0);
+        PlayerPrefs.SetInt("deadBody1Found", 0);
+        PlayerPrefs.SetInt("deadBody2Found", 0);
+        PlayerPrefs.SetInt("deadBody3Found", 0);
+        SceneManager.LoadScene("Credits");
     }
 }
